098678e7c05f2e15aa2a245c98c8730036414eb7
Lourenço Cordeiro <lourenco.cordeiro@diconium.com>
2023-12-27 16:11:18 +0000

    initial commit

---
+++ src/main/java/diconium/cordelou/ghactionspoc/GhActionsPocApplication.java
@@ -0,0 +1,13 @@
+package diconium.cordelou.ghactionspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GhActionsPocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/diconium/cordelou/ghactionspoc/GhActionsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package diconium.cordelou.ghactionspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
