cef8954d4cc27671ea5f8736f35eb962966fa94f
Lourenço Cordeiro <lourenco.cordeiro@diconium.com>
2023-12-28 15:32:47 +0000

    fix test setup

--- src/test/java/diconium/cordelou/ghactionspoc/GhActionsPocApplicationTests.java
+++ src/test/java/diconium/cordelou/ghactionspoc/GhActionsPocApplicationTests.java
@@ -3,7 +3,7 @@ package diconium.cordelou.ghactionspoc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = FakeConfiguration.class)
+@SpringBootTest
 class GhActionsPocApplicationTests {
 
   @Test
